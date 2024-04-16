a920e5eb956e330ddcc24fac46672127b37636e7
CelestialDodo <120543993+CelestialDodo@users.noreply.github.com>
2024-04-15 21:16:55 -0700

    Update script.js

--- MainGame/script.js
+++ MainGame/script.js
@@ -4,7 +4,9 @@ var canClick = true; // Variable to track if the button can be clicked
 
 // Function to enable the button after a delay
 function enableButton() {
-    canClick = true; document.getElementById("clickButton").style.borderColor = "rgb(138, 43, 226)"; document.getElementById("clickButton").removeAttribute("disabled");
+    canClick = true; 
+    document.getElementById("clickButton").removeAttribute("disabled");
+    document.getElementById("clickButton").style.borderColor = "rgb(138, 43, 226)";
 }
 
 // Check if localStorage is supported
@@ -28,17 +30,18 @@ if (typeof(Storage) !== "undefined") {
             document.getElementById("clickCount").textContent = clickCount;
             // Store click count in localStorage
             localStorage.setItem("clickCount", clickCount);
-            
+
             // Disable the button and set canClick to false
             document.getElementById("clickButton").setAttribute("disabled", true);
             canClick = false;
-            
-            // Enable the button after 2 seconds
+
+            // Change border color and enable the button after 2 seconds
             document.getElementById("clickButton").style.borderColor = "rgb(118, 0, 189)";
-            setTimeout(enableButton, 2500);
+            setTimeout(enableButton, 2000);
         }
     });
 } else {
     // Display an error message if localStorage is not supported
     document.getElementById("clickCount").textContent = "Sorry, your browser does not support web storage...";
 }
+
